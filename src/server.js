ccbaa47ae4fc69031a99dc5a0e5e470906a47619
alfonsogamer360 <gutierrezcabrerajeremias@gmail.com>
2026-09-10 16:50:05 -0300

    Conexão com o BD de equipamentos

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { pool } from './database/db.js';
+
+const app = express();
+const port = 3000;
+// Ler o json trafegado na aplicação
+app.use(express.json());
+
+
+app.get("/escola", async (req, res) => {
+    try {
+        const response = await pool.query("SELECT * FROM equipamentos");
+        res.json(response.rows);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Seja bem vindo a minha API: http://localhost:${port}`);
+});
